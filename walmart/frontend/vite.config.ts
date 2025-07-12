2a26a6848572480796512ed8d4d1ba6237a1f0ea
Neethu J <neethu192004@gmail.com>
2025-07-12 19:52:27 +0530

    Update vite.config.ts

--- walmart/frontend/vite.config.ts
+++ walmart/frontend/vite.config.ts
@@ -4,7 +4,6 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/margin-genius/', // ✅ Required for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
@@ -20,4 +19,4 @@ export default defineConfig({
       },
     },
   },
-})
+}) 
